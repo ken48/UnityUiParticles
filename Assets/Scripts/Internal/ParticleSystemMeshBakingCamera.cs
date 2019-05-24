b529a31b951ac962dc4af9b5573e1ace79e46ada
 Kirill Belonogov <k_belonogov@wargaming.net>
2019-05-24 14:37:10 +0300

    File rearranged

---
+++ Assets/Scripts/Internal/ParticleSystemMeshBakingCamera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnitUiParticles.Internal
+{
+    internal class ParticleSystemMeshBakingCamera : MonoBehaviour
+    {
+        static ParticleSystemMeshBakingCamera _instance;
+        Camera _camera;
+        int _refCount;
+
+        public static void RegisterConsumer()
+        {
+            if (_instance == null)
+            {
+                var gameObject = new GameObject(typeof(ParticleSystemMeshBakingCamera).Name);
+
+                // This camera object is just for internal use
+                gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+                _instance = gameObject.AddComponent<ParticleSystemMeshBakingCamera>();
+                _instance._camera = gameObject.AddComponent<Camera>();
+                _instance._camera.orthographic = true;
+
+                // Turn camera off because particle mesh baker will use only camera matrix
+                gameObject.SetActive(false);
+            }
+            _instance._refCount++;
+        }
+
+        public static void UnregisterConsumer()
+        {
+            if (_instance == null)
+                throw new Exception("ParticleSystemMeshBakingCamera has no instance");
+
+            _instance._refCount--;
+            if (_instance._refCount == 0)
+            {
+                if (Application.isPlaying)
+                    Destroy(_instance.gameObject);
+                else
+                    DestroyImmediate(_instance.gameObject);
+                _instance = null;
+            }
+        }
+
+        public static Camera GetCamera(Canvas canvas)
+        {
+            // Adjust camera orthographic size to canvas size
+            // for canvas-based coordinates of particles' size and speed.
+            Vector2 size = ((RectTransform)canvas.transform).rect.size;
+            _instance._camera.orthographicSize = Mathf.Max(size.x, size.y) / canvas.scaleFactor;
+            return _instance._camera;
+        }
+    }
+}
--- Assets/Scripts/ParticleSystemMeshGenerator.cs
+++ Assets/Scripts/ParticleSystemMeshGenerator.cs
@@ -2,157 +2,162 @@
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(ParticleSystem))]
-public class ParticleSystemMeshGenerator : MaskableGraphic
+namespace UnitUiParticles
 {
-    [SerializeField]
-    Material _material;
-    [SerializeField]
-    Material _trailsMaterial;
+    using Internal;
 
-    ParticleSystem _particleSystem;
-    ParticleSystemRenderer _particleSystemRenderer;
-    ParticleSystem.TrailModule _trailsModule;
-    ParticleSystemMeshHelper _meshHelper;
-    Material[] _maskMaterials;
-
-    protected override void Awake()
+    [RequireComponent(typeof(ParticleSystem))]
+    public class ParticleSystemMeshGenerator : MaskableGraphic
     {
-        base.Awake();
+        [SerializeField]
+        Material _material;
+        [SerializeField]
+        Material _trailsMaterial;
 
-        _particleSystem = GetComponent<ParticleSystem>();
-        _particleSystemRenderer = GetComponent<ParticleSystemRenderer>();
-        _trailsModule = _particleSystem.trails;
-    }
+        ParticleSystem _particleSystem;
+        ParticleSystemRenderer _particleSystemRenderer;
+        ParticleSystem.TrailModule _trailsModule;
+        ParticleSystemMeshHelper _meshHelper;
+        Material[] _maskMaterials;
 
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-
-        _meshHelper = ParticleSystemMeshHelper.Create();
-        _maskMaterials = new Material[2];
-
-        ParticleSystemMeshBakingCamera.RegisterConsumer();
-
-        Canvas.willRenderCanvases += Refresh;
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-
-        _meshHelper.Destroy();
-        _meshHelper = null;
-
-        foreach (Material maskMaterial in _maskMaterials)
-            StencilMaterial.Remove(maskMaterial);
-        _maskMaterials = null;
-
-        ParticleSystemMeshBakingCamera.UnregisterConsumer();
-
-        Canvas.willRenderCanvases -= Refresh;
-    }
-
-    protected override void OnValidate()
-    {
-        base.OnValidate();
-
-        GetComponent<ParticleSystemRenderer>().enabled = false;
-    }
-
-    protected override void Reset()
-    {
-        base.Reset();
-
-        raycastTarget = false;
-        GetComponent<ParticleSystemRenderer>().enabled = false;
-        ParticleSystem.MainModule mainModule = GetComponent<ParticleSystem>().main;
-        mainModule.emitterVelocityMode = ParticleSystemEmitterVelocityMode.Transform;
-        mainModule.simulationSpace = ParticleSystemSimulationSpace.Local;
-        mainModule.scalingMode = ParticleSystemScalingMode.Local;
-    }
-
-    void Refresh()
-    {
-        if (canvas == null)
-            return;
-
-        _meshHelper.Clear();
-
-        if (_particleSystem.particleCount > 0)
+        protected override void Awake()
         {
-            Camera meshBakingCamera = ParticleSystemMeshBakingCamera.GetCamera(canvas);
+            base.Awake();
+
+            _particleSystem = GetComponent<ParticleSystem>();
+            _particleSystemRenderer = GetComponent<ParticleSystemRenderer>();
+            _trailsModule = _particleSystem.trails;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _meshHelper = ParticleSystemMeshHelper.Create();
+            _maskMaterials = new Material[2];
+
+            ParticleSystemMeshBakingCamera.RegisterConsumer();
+
+            Canvas.willRenderCanvases += Refresh;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            _meshHelper.Destroy();
+            _meshHelper = null;
+
+            foreach (Material maskMaterial in _maskMaterials)
+                StencilMaterial.Remove(maskMaterial);
+            _maskMaterials = null;
+
+            ParticleSystemMeshBakingCamera.UnregisterConsumer();
+
+            Canvas.willRenderCanvases -= Refresh;
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            GetComponent<ParticleSystemRenderer>().enabled = false;
+        }
+
+        protected override void Reset()
+        {
+            base.Reset();
+
+            raycastTarget = false;
+            GetComponent<ParticleSystemRenderer>().enabled = false;
+            ParticleSystem.MainModule mainModule = GetComponent<ParticleSystem>().main;
+            mainModule.emitterVelocityMode = ParticleSystemEmitterVelocityMode.Transform;
+            mainModule.simulationSpace = ParticleSystemSimulationSpace.Local;
+            mainModule.scalingMode = ParticleSystemScalingMode.Local;
+        }
+
+        void Refresh()
+        {
+            if (canvas == null)
+                return;
+
+            _meshHelper.Clear();
+
+            if (_particleSystem.particleCount > 0)
+            {
+                Camera meshBakingCamera = ParticleSystemMeshBakingCamera.GetCamera(canvas);
+                if (_trailsModule.enabled)
+                {
+                    _particleSystemRenderer.BakeMesh(_meshHelper.GetTemporaryMesh(), meshBakingCamera);
+                    _particleSystemRenderer.BakeTrailsMesh(_meshHelper.GetTemporaryMesh(), meshBakingCamera);
+                    _meshHelper.CombineTemporaryMeshes();
+
+                    if (canvasRenderer.materialCount != 2)
+                        SetMaterialDirty();
+                }
+                else
+                {
+                    _particleSystemRenderer.BakeMesh(_meshHelper.mainMesh, meshBakingCamera);
+
+                    if (canvasRenderer.materialCount != 1)
+                        SetMaterialDirty();
+                }
+            }
+
+            canvasRenderer.SetMesh(_meshHelper.mainMesh);
+        }
+
+        //
+        // Essential overrides
+        //
+
+        protected override void UpdateGeometry()
+        {
+        }
+
+        protected override void OnDidApplyAnimationProperties()
+        {
+        }
+
+        protected override void UpdateMaterial()
+        {
+            canvasRenderer.materialCount = _trailsModule.enabled ? 2 : 1;
+            canvasRenderer.SetMaterial(GetModifiedMaterial(_material, 0), 0);
             if (_trailsModule.enabled)
-            {
-                _particleSystemRenderer.BakeMesh(_meshHelper.GetTemporaryMesh(), meshBakingCamera);
-                _particleSystemRenderer.BakeTrailsMesh(_meshHelper.GetTemporaryMesh(), meshBakingCamera);
-                _meshHelper.CombineTemporaryMeshes();
-
-                if (canvasRenderer.materialCount != 2)
-                    SetMaterialDirty();
-            }
-            else
-            {
-                _particleSystemRenderer.BakeMesh(_meshHelper.mainMesh, meshBakingCamera);
-
-                if (canvasRenderer.materialCount != 1)
-                    SetMaterialDirty();
-            }
+                canvasRenderer.SetMaterial(GetModifiedMaterial(_trailsMaterial, 1), 1);
         }
 
-        canvasRenderer.SetMesh(_meshHelper.mainMesh);
-    }
-
-    //
-    // Essential overrides
-    //
-
-    protected override void UpdateGeometry()
-    {
-    }
-
-    protected override void OnDidApplyAnimationProperties()
-    {
-    }
-
-    protected override void UpdateMaterial()
-    {
-        canvasRenderer.materialCount = _trailsModule.enabled ? 2 : 1;
-        canvasRenderer.SetMaterial(GetModifiedMaterial(_material, 0), 0);
-        if (_trailsModule.enabled)
-            canvasRenderer.SetMaterial(GetModifiedMaterial(_trailsMaterial, 1), 1);
-    }
-
-    // Overloaded version for multiple materials
-    Material GetModifiedMaterial(Material baseMaterial, int index)
-    {
-        Material baseMat = baseMaterial;
-        if (m_ShouldRecalculateStencil)
+        // Overloaded version for multiple materials
+        Material GetModifiedMaterial(Material baseMaterial, int index)
         {
-            m_ShouldRecalculateStencil = false;
-
-            if (maskable)
+            Material baseMat = baseMaterial;
+            if (m_ShouldRecalculateStencil)
             {
-                Transform sortOverrideCanvas = MaskUtilities.FindRootSortOverrideCanvas(transform);
-                m_StencilValue = MaskUtilities.GetStencilDepth(transform, sortOverrideCanvas) + index;
+                m_ShouldRecalculateStencil = false;
+
+                if (maskable)
+                {
+                    Transform sortOverrideCanvas = MaskUtilities.FindRootSortOverrideCanvas(transform);
+                    m_StencilValue = MaskUtilities.GetStencilDepth(transform, sortOverrideCanvas) + index;
+                }
+                else
+                {
+                    m_StencilValue = 0;
+                }
             }
-            else
+
+            Mask component = GetComponent<Mask>();
+            if (m_StencilValue > 0 && (component == null || !component.IsActive()))
             {
-                m_StencilValue = 0;
+                int stencilId = (1 << m_StencilValue) - 1;
+                Material maskMaterial = StencilMaterial.Add(baseMat, stencilId, StencilOp.Keep, CompareFunction.Equal,
+                    ColorWriteMask.All, stencilId, 0);
+                StencilMaterial.Remove(_maskMaterials[index]);
+                _maskMaterials[index] = maskMaterial;
+                baseMat = _maskMaterials[index];
             }
-        }
 
-        Mask component = GetComponent<Mask>();
-        if (m_StencilValue > 0 && (component == null || !component.IsActive()))
-        {
-            int stencilId = (1 << m_StencilValue) - 1;
-            Material maskMaterial = StencilMaterial.Add(baseMat, stencilId, StencilOp.Keep, CompareFunction.Equal,
-                ColorWriteMask.All, stencilId, 0);
-            StencilMaterial.Remove(_maskMaterials[index]);
-            _maskMaterials[index] = maskMaterial;
-            baseMat = _maskMaterials[index];
+            return baseMat;
         }
-
-        return baseMat;
     }
 }
